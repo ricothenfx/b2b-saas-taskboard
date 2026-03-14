b4c94235adf0f8c1aff05b67b20c6bae9d106104
ricothenfx <ricothenfx@gmail.com>
2026-03-14 13:30:19 +0700

    migrate sqlite to postgresql

--- backend/app/core/database.py
+++ backend/app/core/database.py
@@ -3,8 +3,7 @@ from sqlalchemy.orm import sessionmaker, declarative_base
 from app.core.config import settings
 
 engine = create_engine(
-    settings.DATABASE_URL,
-    connect_args={"check_same_thread": False}
+    settings.DATABASE_URL
 )
 
 SessionLocal = sessionmaker(
--- backend/start.py
+++ backend/start.py
@@ -4,3 +4,5 @@ import uvicorn
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 10000))
     uvicorn.run("app.main:app", host="0.0.0.0", port=port)
+
+    #uvicorn.run("app.main:app", host="127.0.0.1", port=8000, reload=True)
